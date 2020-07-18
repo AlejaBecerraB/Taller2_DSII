7199df1f9570e485c563ffce272f0e070dae29dd
Alejandra Becerra <Alejandra Becerra@DESKTOP-LUQ0G79>
2020-07-17 20:01:38 -0500

    commit #2 Alejandra

---
+++ src/Controlador/TallerDS_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author Alejandra Becerra
+ */
+public class TallerDS_2 {
+
+    public TallerDS_2() {
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
